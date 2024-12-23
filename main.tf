0aacc3dd40814d7c9d3f440680dac0fb257cb0a5
Haru <97601751+HaruChebrolu@users.noreply.github.com>
2024-12-23 12:33:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,39 +57,39 @@ module "route_table" {
   vpc_id = module.vpc.vpc_id
   route_table_config = {
     route_table1 = {
-      name      = "route-table-one"
+      name      = "apical-prod-integration-rtb-private-ap-southeast-1a"
       subnet_id = module.subnet.subnet_ids["subnet1"]
     }
     route_table2 = {
-      name      = "route-table-two"
+      name      = "apical-prod-integration-rtb-private-ap-southeast-1b"
       subnet_id = module.subnet.subnet_ids["subnet2"]
     }
     route_table3 = {
-      name      = "route-table-three"
+      name      = "apical-prod-integration-rtb-private-ap-southeast-1c"
       subnet_id = module.subnet.subnet_ids["subnet3"]
     }
     route_table4 = {
-      name      = "route-table-four"
+      name      = "apical-prod-workload-rtb-private-ap-southeast-1a"
       subnet_id = module.subnet.subnet_ids["subnet4"]
     }
     route_table5 = {
-      name      = "route-table-five"
+      name      = "apical-prod-workload-rtb-private-ap-southeast-1b"
       subnet_id = module.subnet.subnet_ids["subnet5"]
     }
     route_table6 = {
-      name      = "route-table-six"
+      name      = "apical-prod-workload-rtb-private-ap-southeast-1c"
       subnet_id = module.subnet.subnet_ids["subnet6"]
     }
     route_table7 = {
-      name      = "route-table-seven"
+      name      = "apical-prod-transit-rtb-private-ap-southeast-1a"
       subnet_id = module.subnet.subnet_ids["subnet7"]
     }
     route_table8 = {
-      name      = "route-table-eight"
+      name      = "apical-prod-transit-rtb-private-ap-southeast-1b"
       subnet_id = module.subnet.subnet_ids["subnet8"]
     }
     route_table9 = {
-      name      = "route-table-nine"
+      name      = "apical-prod-transit-rtb-private-ap-southeast-1c"
       subnet_id = module.subnet.subnet_ids["subnet9"]
     }
   }
